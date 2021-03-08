dbf8cf443ce895b2fad6fbfd751f5357658b3987
Emma Munachi <victornothector@gmail.com>
2021-03-08 10:49:58 +0100

    delete tasks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
+import React, { useState } from "react";
 import './App.css';
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState(
+    [
+      {
+        id: 1,
+        text: "Kill Thanos",
+        day: "3rd March at 10:00am",
+        reminder: true
+      },
+      {
+        id: 2,
+        text: "Save the world",
+        day: "7th January at 12:00am",
+        reminder: true
+      },
+      {
+        id: 3,
+        text: "make a sandwich",
+        day: "1st July at 9:00pm",
+        reminder: false
+      }
+    ]
+  )
+
+    // Delete task
+    const deleteTask = (id) => {
+      setTasks(tasks.filter((task) => task.id !== id))
+    }
+
+
   return (
-    <div>
+    <div className="container">
      <Header title="Task Tracker"/>
-     <Tasks />
+     {tasks.length > 0 ? (
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
+      ) : ( "No tasks to show." )
+     }
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,33 +1,12 @@
 import React from 'react';
+import Task from "./Task";
 
-const Tasks = () => {
-
-  const tasks = [
-    {
-      id: 1,
-      text: "Kill Thanos",
-      day: "3rd March at 10:00am",
-      reminder: true
-    },
-    {
-      id: 2,
-      text: "Save the world",
-      day: "7th January at 12:00am",
-      reminder: true
-    },
-    {
-      id: 3,
-      text: "make a sandwich",
-      day: "1st July at 9:00pm",
-      reminder: false
-    }
-  ]
-
+const Tasks = ({tasks, onDelete}) => {
 
   return (
     <div>
       {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+      <Task key={task.id} task={task} onDelete={onDelete}/>
       ))}
     </div>
   )
